ea869c7ff825d975d7a19dbec17f356861b471de
Mikolaj Nalecz <mikolaj.nalecz@hotmail.com>
2018-07-09 11:01:59 +0200

    Added modal.

--- src/components/LateFees.vue
+++ src/components/LateFees.vue
@@ -14,14 +14,14 @@
     <div v-show="this.detailView.przetrzymanePrzedmioty.length > 0">
       <h2>Informacje na temat opóznień użytkownika {{detailView.imie}} {{detailView.nazwisko}}</h2>
       <br><br>
-      <b-table :items="detailView.przetrzymanePrzedmioty" :fields="detailFields" ></b-table>
+      <b-table :items="detailView.przetrzymanePrzedmioty" :fields="detailFields"></b-table>
       <br><br>
       <b-row>
         <b-col>
           <h3>SALDO: <b>{{detailView.saldo}}</b></h3>
         </b-col>
         <b-col>
-          <b-button variant="danger" size="xl" @click.stop="sendLateFeesEmail()" class="mr-2">
+          <b-button v-b-modal.myModal variant="danger" size="xl" @click.stop="" class="mr-2">
             WYSLIJ PONAGLENIE
           </b-button>
         </b-col>
@@ -29,7 +29,10 @@
 
     </div>
 
-      </div>
+    <b-modal id="myModal" @ok="sendLateFeesEmail()">
+      Czy na pewno wysłać ponaglenie użytkownikowi?
+    </b-modal>
+  </div>
 </template>
 
 <script>
@@ -39,7 +42,7 @@
   export default {
     name: 'LateFees',
 
-    data () {
+    data() {
       return {
         detailView: {
           firstName: "",
@@ -60,12 +63,12 @@
         items: []
       }
     },
-    mounted: function() {
+    mounted: function () {
       this.getLateFees();
     },
     methods: {
       // Fetches posts when the component is created.
-      getLateFees () {
+      getLateFees() {
         AXIOS.get(`/lateFees`)
           .then(response => {
             console.log(response);
@@ -73,7 +76,7 @@
           })
       },
       sendLateFeesEmail() {
-        AXIOS.post(`/sendLateFeesEmail`, {id : this.detailView.id})
+        AXIOS.post(`/sendLateFeesEmail`, {id: this.detailView.id})
           .then(response => {
             console.log(response);
           })
