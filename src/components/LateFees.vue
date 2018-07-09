1782198d740f5fd9999eb0a16d9424da2b21a2c9
Mikolaj Nalecz <mikolaj.nalecz@hotmail.com>
2018-07-09 03:30:21 +0200

    Finished.

--- src/components/LateFees.vue
+++ src/components/LateFees.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="LateFees">
-    <h1>Informacje na temat spoznionych zwrotow</h1>
+    <h2>Informacje na temat spóznionych zwrotów</h2>
     <br>
     <br>
-    <b-table :items="items" :fields="fields">
-      <template slot="show_details" slot-scope="row">
-        <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
+    <b-table :items="items" :fields="masterFields">
+      <template slot="detale" slot-scope="row">
         <b-button size="sm" @click.stop="detailView = row.item" class="mr-2">
-          Details
+          WYBIERZ
         </b-button>
       </template>
     </b-table>
-
+    <br>
     <div v-show="this.detailView.przetrzymanePrzedmioty.length > 0">
-      <h2>Informacje na temat opoznien uzytkownika {{detailView.imie}} {{detailView.nazwisko}}</h2>
+      <h2>Informacje na temat opóznień użytkownika {{detailView.imie}} {{detailView.nazwisko}}</h2>
       <br><br>
-      <b-table :items="detailView.przetrzymanePrzedmioty" :fields="fields2" ></b-table>
+      <b-table :items="detailView.przetrzymanePrzedmioty" :fields="detailFields" ></b-table>
       <br><br>
       <b-row>
         <b-col>
-          <h3>SALDO: {{detailView.saldo}}</h3>
+          <h3>SALDO: <b>{{detailView.saldo}}</b></h3>
         </b-col>
         <b-col>
-          <b-button variant="danger" size="xl" @click.stop="" class="mr-2">
+          <b-button variant="danger" size="xl" @click.stop="sendLateFeesEmail()" class="mr-2">
             WYSLIJ PONAGLENIE
           </b-button>
         </b-col>
       </b-row>
 
     </div>
-
 
       </div>
 </template>
@@ -43,24 +41,22 @@
 
     data () {
       return {
-        response: [],
         detailView: {
           firstName: "",
           lastName: "",
           przetrzymanePrzedmioty: [],
-          saldo: ""
+          saldo: "",
+          id: ""
         },
-        errors: [],
-        user: {
-          lastName: '',
-          firstName: '',
-          id: 0
-        },
-        showResponse: false,
-        retrievedUser: {},
-        showRetrievedUser: false,
-        fields: [ 'imie', 'nazwisko', 'saldo', 'show_details' ],
-        fields2: [ 'autor', 'nazwa', 'dataTeoretycznegoOddania'],
+        masterFields: [
+          {key: 'imie', sortable: true},
+          {key: 'nazwisko', sortable: true},
+          {key: 'saldo', sortable: true},
+          {key: 'detale', sortable: false}],
+        detailFields: [
+          {key: 'autor', sortable: true},
+          {key: 'nazwa', sortable: true},
+          {key: 'dataTeoretycznegoOddania', sortable: true}],
         items: []
       }
     },
@@ -75,33 +71,14 @@
             console.log(response);
             this.items = response.data;
           })
-          .catch(e => {
-            this.errors.push(e)
+      },
+      sendLateFeesEmail() {
+        AXIOS.post(`/sendLateFeesEmail`, {id : this.detailView.id})
+          .then(response => {
+            console.log(response);
           })
       }
     }
   }
 
 </script>
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-</style>
